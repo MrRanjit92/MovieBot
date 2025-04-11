a362916581ce6a500be041dd1d9f90764aaa0ef6
MrRanjit92 <127598781+MrRanjit92@users.noreply.github.com>
2025-04-11 14:20:47 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,24 +1,32 @@
-"""distutils
+"""distutils.command
 
-The main package for the Python Module Distribution Utilities.  Normally
-used from a setup script as
+Package containing implementation of all the standard Distutils
+commands."""
 
-   from distutils.core import setup
-
-   setup (...)
-"""
-
-import sys
-import importlib
-
-__version__ = sys.version[: sys.version.index(' ')]
-
-
-try:
-    # Allow Debian and pkgsrc (only) to customize system
-    # behavior. Ref pypa/distutils#2 and pypa/distutils#16.
-    # This hook is deprecated and no other environments
-    # should use it.
-    importlib.import_module('_distutils_system_mod')
-except ImportError:
-    pass
+__all__ = [
+    'build',
+    'build_py',
+    'build_ext',
+    'build_clib',
+    'build_scripts',
+    'clean',
+    'install',
+    'install_lib',
+    'install_headers',
+    'install_scripts',
+    'install_data',
+    'sdist',
+    'register',
+    'bdist',
+    'bdist_dumb',
+    'bdist_rpm',
+    'bdist_wininst',
+    'check',
+    'upload',
+    # These two are reserved for future use:
+    #'bdist_sdux',
+    #'bdist_pkgtool',
+    # Note:
+    # bdist_packager is not included because it only provides
+    # an abstract base class
+]
